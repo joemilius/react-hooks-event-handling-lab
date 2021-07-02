871fef0d72af9bd97bee402787140c51b7bcfaff
JoeMilius <joemilius@gmail.com>
2021-07-02 16:36:43 -0600

    Completed Assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
-// Code EyesOnMe Component Here
+function EyesOnMe(){
+
+    // function handleFocus(event){
+    //      if (event === true){
+    //          console.log('Good!')
+    //      }else {
+    //         console.log('Hey! Eyes on me!')
+    //      }   
+    // }
+    // function handleBlur(){
+    //      console.log('Hey! Eyes on me!') 
+    // }
+
+    return(
+        <button onFocus={() => console.log('Good!')} onBlur={() => console.log('Hey! Eyes on me!')}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe
